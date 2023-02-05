e2785971debbdef680f3f1e23431ad4ec7f9231c
Nitish Goswami <ni30.info@gmail.com>
2023-02-05 10:16:53 +0530

    Intro : Variable,type,comparison

---
+++ intro.js
@@ -0,0 +1,31 @@
+// variable intro
+var a = 10;
+console.log(a);
+
+// Loose type : We can assign any type of value at diffrent time
+
+a = "Nitish"; // String Assignment
+console.log(a);
+
+a = true; //Boolean assignment
+console.log(a);
+
+//null
+var  b = null;
+console.log(b);
+
+var c;
+console.log(c);
+
+
+// Get datatype of variable or value
+console.log(typeof 10);
+console.log(typeof 10.00);
+console.log(typeof "10");
+console.log(typeof '10');
+
+// == : Check if value are equal
+console.log(1 == '1')
+
+// === : Check value and type
+console.log(1 === '1')
