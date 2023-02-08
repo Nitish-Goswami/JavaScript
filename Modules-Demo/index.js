d575da04f1cd268cedede2a46d0bb159221573c6
Nitish Goswami <ni30.info@gmail.com>
2023-02-08 12:43:53 +0530

    Modules Intro

---
+++ Modules-Demo/index.js
@@ -0,0 +1,9 @@
+const Operations = require("./Operations");
+
+console.log("Module Demo");
+let a = 5, b = 2;
+
+console.log(Operations.add(a,b))
+console.log(Operations.mul(a,b))
+console.log(Operations.sub(a,b))
+console.log(Operations.div(a,b))
